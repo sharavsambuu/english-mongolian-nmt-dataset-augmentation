234c347ce8d10d7ab955b0c40f9f3a6e79878b03
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-09-08 10:47:54 +0800

    fix writing translation result

--- translate_sentences.sh
+++ translate_sentences.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+rm mn_en_sentences.txt && touch mn_en_sentences.txt
+
 translate() {
   mn_sentence="$1"
-  en_sentence=$(torify trans $mn_sentence)
-  echo "$sentence", "+++++SEP+++++", "$en_sentence"
+  en_sentence=$(torify trans -brief "$mn_sentence")
+  echo "$mn_sentence", "+++++SEP+++++", "$en_sentence" >> mn_en_sentences.txt
 }
 
 export -f translate
 
-cat filtered_sentences.txt | parallel -j0 -k translate >> mn_en_sentences.txt
+cat filtered_sentences.txt | parallel -j0 -k translate 
 
 
