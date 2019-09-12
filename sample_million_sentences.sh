aaf346ed6cf4af00faa31119cabf0ae553676e27
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-09-12 12:10:02 +0000

    little bit shuffling on sampled sentences

--- sample_million_sentences.sh
+++ sample_million_sentences.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 rm sampled_million_sentences.txt && touch sampled_million_sentences.txt
+rm sampled_million_uniq_sentences.txt && touch sampled_million_uniq_sentences.txt
 
 echo "sampling process beings..."
 
 ls ./sentences_parts -I "mn_en_sentences_split.txtaa" | while read file; do
 	filepath="./sentences_parts/$file"
 	echo "sampling from $filepath ..."
-	sort -u $filepath | sort -R | head -n 20000 >> sampled_million_sentences.txt
+	sort -u $filepath | shuf -n 20000 >> sampled_million_sentences.txt
 done
+
+cat sampled_million_sentences.txt | sort -u | shuf > sampled_million_uniq_sentences.txt
 
 echo "sampling is done."
