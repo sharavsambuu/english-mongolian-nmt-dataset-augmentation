78be00d27c28783c8f5638f8edf468cd603b683e
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-09-12 11:45:12 +0000

    Unique 1m sampler. cat sampled_million_sentences.txt | sort -u > sampled_million_uniq_sentences.txt

--- sample_million_sentences.sh
+++ sample_million_sentences.sh
@@ -7,7 +7,7 @@ echo "sampling process beings..."
 ls ./sentences_parts -I "mn_en_sentences_split.txtaa" | while read file; do
 	filepath="./sentences_parts/$file"
 	echo "sampling from $filepath ..."
-	sort -R $filepath | head -n 20000 >> sampled_million_sentences.txt
+	sort -u $filepath | sort -R | head -n 20000 >> sampled_million_sentences.txt
 done
 
 echo "sampling is done."
