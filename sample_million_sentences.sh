03f8bd654afc4ed091dfc2c13e5ef72e363a8fdf
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-09-12 11:05:58 +0000

    one million sentences sampler

--- sample_million_sentences.sh
+++ sample_million_sentences.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm sampled_200k_sentences.txt && touch sampled_200k_sentences.txt
+rm sampled_million_sentences.txt && touch sampled_million_sentences.txt
 
 echo "sampling process beings..."
 
 ls ./sentences_parts -I "mn_en_sentences_split.txtaa" | while read file; do
 	filepath="./sentences_parts/$file"
-	echo "sampleing from $filepath ..."
-	sort -R $filepath | head -n 4000 >> sampled_200k_sentences.txt
+	echo "sampling from $filepath ..."
+	sort -R $filepath | head -n 20000 >> sampled_million_sentences.txt
 done
 
 echo "sampling is done."
